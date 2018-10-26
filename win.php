0b260e5b0e6b280f603d60da302d184e2770592f
Vlad_Ekersdorf <nerfylaw@mail.ru>
2018-10-26 18:29:26 +0300

    !!!

--- win.php
+++ win.php
@@ -4,6 +4,6 @@ $app = new \atk4\ui\App('Угадайка');
 $app->initLayout('Centered');
 $good= $_GET['good'];
 $label = $app->add(['Label',$good,'massive green']);
-$header = $app->add(['Header','Отлично!']);
+$header = $app->add(['Header','Отлично!!!!!']);
 $button4= $app->add (['Button','Начать заново']);
 $button4->link(['index']);
